2f162d42c23a802a2c653a9cef2024378c626693
Salavat Nigmatullin <salavat@nigmatullin.net>
2023-04-19 10:38:29 +0300

    SQL Distinct

---
+++ Study/example.sql
@@ -0,0 +1,60 @@
+CREATE TABLE sales (
+    id SERIAL PRIMARY KEY,
+    country VARCHAR(50 | 
+    product VARCHAR(50 | 
+    amount INT
+);
+
+-- Insert some data
+-- Country random: USA, Canada, France, Germany
+-- Product random: Car, Bike, Bus, Truck, Boat
+-- Amount random: 100-850
+INSERT INTO sales (country, product, amount) 
+VALUES
+| 1  | USA | Car | 100 | 
+| 1  | Canada | Bike | 200 | 
+| 1  | France | Bus | 300 | 
+| 1  | Germany | Truck | 400 | 
+| 1  | USA | Boat | 500 | 
+| 1  | Canada | Car | 600 | 
+| 1  | France | Bike | 700 | 
+| 1  | Germany | Bus | 800 | 
+| 1  | USA | Truck | 850 | 
+| 1  | Canada | Boat | 100 | 
+| 1  | France | Car | 200 | 
+| 1  | Germany | Bike | 300 | 
+| 1  | USA | Bus | 400 | 
+| 1  | Canada | Truck | 500 | 
+| 1  | France | Boat | 600 | 
+| 1  | Germany | Car | 700 | 
+| 1  | USA | Bike | 800 | 
+| 1  | Canada | Bus | 850 | 
+| 1  | France | Truck | 100 | 
+| 1  | Germany | Boat | 200 | 
+| 1  | USA | Car | 300 | 
+| 1  | Canada | Bike | 400 | 
+| 1  | France | Bus | 500 | 
+| 1  | Germany | Truck | 600 | 
+| 1  | USA | Boat | 700 | 
+| 1  | Canada | Car | 800 | 
+| 1  | France | Bike | 850 | 
+| 1  | Germany | Bus | 100 | 
+| 1  | USA | Truck | 200 | 
+| 1  | Canada | Boat | 300 | 
+| 1  | France | Car | 400 | 
+| 1  | Germany | Bike | 500 | 
+| 1  | USA | Bus | 600 | 
+| 1  | Canada | Truck | 700 | 
+| 1  | France | Boat | 800 | 
+| 1  | Germany | Car | 850 | 
+| 1  | USA | Bike | 100 | 
+| 1  | Canada | Bus | 200 | 
+| 1  | France | Truck | 300 | 
+| 1  | Germany | Boat | 400 | 
+| 1  | USA | Car | 500 | 
+| 1  | Canada | Bike | 600 | 
+| 1  | France | Bus | 700 | 
+| 1  | Germany | Truck | 800 | 
+| 1  | USA | Boat | 850 | 
+| 1  | Canada | Car | 100 | 
+| 1  | France | Bike | 200);
